de5050e72e3785ea68668fe6a8df15c4a7f3df2d
Rodrigo <rhbon.08@gmail.com>
2024-03-11 19:35:32 -0500

    Compilar todos los archivos SCSS

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const sass = require("gulp-sass")(require('sass'));
 
 function css(done) {
 
-  src("src/scss/app.scss") // Identificar el archivo SASS
+  src("src/scss/**/*.scss") // Identificar el archivo SASS
     .pipe(sass()) // Compilarlo
     .pipe(dest("build/css")); // Almacenarla en el dico duro
 
@@ -11,7 +11,7 @@ function css(done) {
 }
 
 function dev(done) {
-  watch("src/scss/app.scss", css)
+  watch("src/scss/**/*.scss", css)
 
   done();
 }
