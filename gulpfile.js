82d8c11bca1f8aeb5336d5abe00b53d31bea0ea0
Rodrigo <rhbon.08@gmail.com>
2024-03-19 18:58:51 -0500

    aligerar imagenes con gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,10 @@ const { src, dest, watch, parallel } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require("gulp-plumber");
 
+//
+const cache = require("gulp-cache");
+const imagemin = require("gulp-imagemin");
+
 function css(done) {
   src("src/scss/**/*.scss") // Identificar el archivo SASS
     .pipe(plumber())
@@ -14,32 +18,46 @@ function css(done) {
   done(); // Callback que avisa a gulp cuando llegamos al final
 }
 
+// Imagenes
+
+function imagenes(done) {
+  const options = {
+    optimizationLevel: 3,
+  };
+
+  src("src/img/**/*.{png, jpg}")
+    .pipe(cache(imagemin(options)))
+    .pipe(dest("build/img"));
+  done();
+}
+
 function versionWebp(done) {
-  import('gulp-webp').then(module => {
-    const webp = module.default; // Acceder a la exportación predeterminada del módulo
+  import("gulp-webp")
+    .then((module) => {
+      const webp = module.default; // Acceder a la exportación predeterminada del módulo
 
-    const options = {
-      quality: 50,
-    };
+      const options = {
+        quality: 50,
+      };
 
-    src("src/img/**/*.{png, jpg}")
-      .pipe( webp(options))
-      .pipe( dest("build/img"))
-  
-    done();
-  }).catch(error => {
-    console.log("Error importando gulp-webp", error);
-    donde(error); // Pasar el error para indicar falla en la tarea
-  })
+      src("src/img/**/*.{png,jpg}")
+        .pipe(webp(options))
+        .pipe(dest("build/img"));
 
+      done();
+    })
+    .catch((error) => {
+      console.log("Error importando gulp-webp", error);
+      donde(error); // Pasar el error para indicar falla en la tarea
+    });
 }
 
 function dev(done) {
   watch("src/scss/**/*.scss", css);
-
   done();
 }
 
 exports.css = css;
+exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
-exports.dev = parallel(versionWebp, dev);
+exports.dev = parallel(imagenes, versionWebp, dev);
