163bd9eb13e9cffbf2031425e38473081a7ebb63
Rodrigo <rhbon.08@gmail.com>
2024-03-20 15:30:17 -0500

    Generando el HTML de la galeria, junto con un script para mostrarla

--- build/js/app.js
+++ build/js/app.js
@@ -9,5 +9,15 @@ function iniciarApp() {
 function crearGaleria() {
   const galeria = document.querySelector(".image-gallery");
 
-  galeria.textContent = "Vamos a crear la galria";
+  for (let i = 1; i <= 12; i++) {
+    const imagen = document.createElement("picture");
+    imagen.innerHTML = `
+      <source srcset="build/img/thumb/${i}.avif" type="imagen/avif" />
+      <source srcset="build/img/thumb/${i}.webp" type="imagen/webp" />
+      <img loading="lazy" width="200" height="300" src="build/img/thumb/${i}.jpg"
+        alt="imagen galeria">
+    `;
+
+    galeria.appendChild(imagen);
+  }
 }
