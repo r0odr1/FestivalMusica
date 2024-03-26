7c9974f21be3c909f25e715b6b480402d288fae7
Rodrigo <rhbon.08@gmail.com>
2024-03-26 16:50:02 -0500

    Añadiendo un botón para cerrar la imagen

--- src/js/app.js
+++ src/js/app.js
@@ -34,10 +34,29 @@ function mostrarImagen(id) {
         alt="imagen galeria">
     `;
 
+  // Crea el overlay con la imagen
   const overlay = document.createElement("DIV");
   overlay.appendChild(imagen);
-  overlay.classList.add('overlay');
+  overlay.classList.add("overlay");
+  overlay.onclick = function () {
+    const body = document.querySelector("body");
+    body.classList.remove("fijar-body");
+    overlay.remove();
+  };
 
-  const body = document.querySelector('body');
+  // Boton para cerra la ventana modal
+  const cerrarModal = document.createElement("P");
+  cerrarModal.textContent = "X";
+  cerrarModal.classList.add("btn-cerrar");
+  cerrarModal.onclick = function () {
+    const body = document.querySelector("body");
+    body.classList.remove("fijar-body");
+    overlay.remove();
+  };
+  overlay.appendChild(cerrarModal);
+
+  // Añadirlo al HTML
+  const body = document.querySelector("body");
   body.appendChild(overlay);
+  body.classList.add("fijar-body");
 }
