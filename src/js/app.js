721cfc699e0d01fb83bd4d71007be0d05c8eae25
Rodrigo <rhbon.08@gmail.com>
2024-03-26 16:15:46 -0500

    Detectando la imagen a la que le doy click para mostrarla en una versión mas grande

--- src/js/app.js
+++ src/js/app.js
@@ -17,7 +17,27 @@ function crearGaleria() {
       <img loading="lazy" width="200" height="300" src="build/img/thumb/${i}.jpg"
         alt="imagen galeria">
     `;
+    imagen.onclick = function () {
+      mostrarImagen(i);
+    };
 
     galeria.appendChild(imagen);
   }
+}
+
+function mostrarImagen(id) {
+  const imagen = document.createElement("picture");
+  imagen.innerHTML = `
+      <source srcset="build/img/grande/${id}.avif" type="imagen/avif" />
+      <source srcset="build/img/grande/${id}.webp" type="imagen/webp" />
+      <img loading="lazy" width="200" height="300" src="build/img/grande/${id}.jpg"
+        alt="imagen galeria">
+    `;
+
+  const overlay = document.createElement("DIV");
+  overlay.appendChild(imagen);
+  overlay.classList.add('overlay');
+
+  const body = document.querySelector('body');
+  body.appendChild(overlay);
 }
